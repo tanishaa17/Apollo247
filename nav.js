d0a69a99db7e13b09014c78fab4ab3e79687d5e5
SakethReddy1111 <sakethr007@gmail.com>
2021-12-15 20:47:01 +0530

    save
    save

---
+++ nav.js
@@ -0,0 +1,11 @@
+document.querySelector("#doc").addEventListener("click", navdoc);
+
+function navdoc() {
+  window.location.href = "doctors.html";
+}
+
+document.querySelector("#pharma").addEventListener("click", navpharma);
+
+function navpharma() {
+  window.location.href = "pharmacy.html";
+}
