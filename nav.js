06c0ab469c4a6cb99a51b1dabb4419673357def9
SakethReddy1111 <sakethr007@gmail.com>
2021-12-16 10:30:22 +0530

    changes
    js

---
+++ nav.js
@@ -0,0 +1,17 @@
+document.querySelector("#apolloimage").addEventListener("click", navhome);
+
+function navhome() {
+  window.location.href = "home page.html";
+}
+
+document.querySelector("#doc").addEventListener("click", navdoc);
+
+function navdoc() {
+  window.location.href = "doctors.html";
+}
+
+document.querySelector("#pharma").addEventListener("click", navpharma);
+
+function navpharma() {
+  window.location.href = "pharmacy.html";
+}
