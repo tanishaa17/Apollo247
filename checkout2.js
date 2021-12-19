afea2fbf0de5da85e58b252bcf8c551e991e3d09
SakethReddy1111 <sakethr007@gmail.com>
2021-12-19 16:06:25 +0530

    Update checkout2.js

--- checkout2.js
+++ checkout2.js
@@ -5,4 +5,9 @@ function display() {
   document.querySelector("#clear").textContent = dvdetails.name;
   document.querySelector("#clear1").textContent = dvdetails.address;
   document.querySelector("#clear2").textContent = dvdetails.number;
+
+  var addedToCart = JSON.parse(localStorage.getItem("cartItemsadded"));
+  addedToCart = [];
+
+  localStorage.setItem("cartItemsadded", JSON.stringify(addedToCart));
 }
